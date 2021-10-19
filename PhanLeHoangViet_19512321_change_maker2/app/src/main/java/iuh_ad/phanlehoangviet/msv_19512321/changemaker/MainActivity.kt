4af6336dc7e06312c1dab3862f85ff2c61b6e09f
VQHoangViet <nhockpopi6@gmail.com>
2021-10-19 17:21:01 +0700

    homework_version1

---
+++ PhanLeHoangViet_19512321_change_maker2/app/src/main/java/iuh_ad/phanlehoangviet/msv_19512321/changemaker/MainActivity.kt
@@ -0,0 +1,11 @@
+package iuh_ad.phanlehoangviet.msv_19512321.changemaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
